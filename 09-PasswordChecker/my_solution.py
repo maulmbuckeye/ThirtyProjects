86f332bb7587f0d1111d74008a283d1ee7efc08f
MIke Ulm <maulmbuckeye@gmail.com>
2023-08-13 20:03:55 -0400

    BadPassword class passes 5 unit and 3 integration tests

--- 09-PasswordChecker/my_solution.py
+++ 09-PasswordChecker/my_solution.py
@@ -1,13 +1,39 @@
+class PasswordResult:
 
-def check_password(possible_password: str) -> bool:
+    def __init__(self, result: bool, msg=''):
+        self._result = result
+        self._msg = msg
+
+
+class BadPasswords:
+
+    def __init__(self, bad_passwords: list[str]):
+        self._bad = bad_passwords
+
+    def is_ok(self, a_candidate: str) -> bool:
+        return a_candidate in self._bad
+
+    def is_ok_and_rank(self, a_candidate: str) -> tuple[bool, str]:
+        for i, common_password in enumerate(self._bad, start=1):
+            if a_candidate == common_password:
+                return False, str(i)
+        return True, ''
+
+
+def password_check(possible_password: str,
+                   list_of_bad: str = "passwords.text") -> tuple[bool, str]:
+    """Return (True, '') if password is okay,
+    otherwise return (False, "reason").
+
+    Reasons include:
+        '3' -> 3rd on bad password list
+    """
     try:
-        with open("passwords.text") as file:
-            unacceptable_passwords: list[str] = file.read().splitlines()
+        with open(list_of_bad) as file:
+            bp = BadPasswords(file.read().splitlines())
     except Exception as _:
-        unacceptable_passwords = ["password", "passwd", "mike_ulm"]
-    if possible_password in unacceptable_passwords:
-        return False
-    return True
+        bp = BadPasswords(["password", "passwd", "mike_ulm"])
+    return bp.is_ok_and_rank(possible_password)
 
 
 def request_and_check_password():
--- 09-PasswordChecker/test_check_password.py
+++ 09-PasswordChecker/test_check_password.py
@@ -1,28 +1,47 @@
 import unittest
 from unittest.mock import patch, mock_open
-from my_solution import check_password
+from my_solution import password_check
 
 
-class MyTestCase(unittest.TestCase):
+class MyUnitTests(unittest.TestCase):
 
-    def test_an_okay_password(self):
-        self.assertTrue(check_password("ok_password"))
-
-    def test_password_is_not_okay(self):
-        self.assertFalse(check_password("password"))
-
-    def test_passwd_is_not_okay(self):
-        self.assertFalse(check_password("passwd"))
-
-    def test_mike_ulm_is_not_okay(self):
+    def test_password_is_first_on_internal_list(self):
+        """ Checks that 'ting' is not in no-file-found list."""
         with patch("my_solution.open",
                    side_effect=FileNotFoundError):
-            self.assertFalse(check_password("mike_ulm"))
+            self.assertEqual((False, '1'), password_check("password"))
+
+    def test_passwd_is_not_okay(self):
+        self.assertFalse(password_check("passwd")[0])
+
+    def test_ting_is_okay_if_no_file(self):
+        """ Checks that 'ting' is not in no-file-found list."""
+        with patch("my_solution.open",
+                   side_effect=FileNotFoundError):
+            self.assertEqual((True, ''), password_check("ting"))
+
+    def test_mike_ulm_is_not_okay_if_no_file(self):
+        with patch("my_solution.open",
+                   side_effect=FileNotFoundError):
+            self.assertEqual((False, '3'), password_check("mike_ulm"))
 
     def test_if_no_file(self):
         with patch("my_solution.open",
                    side_effect=FileNotFoundError):
-            self.assertFalse(check_password("password"))
+            self.assertEqual((False, "1"), password_check("password"))
+
+
+class MyIntegrationTests(unittest.TestCase):
+    def test_password_is_not_okay(self):
+        self.assertEqual((False, '2'), password_check("password"))
+
+    def test_an_okay_password(self):
+        self.assertEqual((True, ''), password_check("ok_password"))
+
+    def test_if_no_file(self):
+        result = password_check("password",
+                                list_of_bad="does_not_exist.txt")
+        self.assertEqual((False, "1"), result)
 
 
 if __name__ == '__main__':
