c3710a56239f64d8ef86750360e0ff5ccdc8bec4
MIke Ulm <maulmbuckeye@gmail.com>
2023-08-14 10:42:25 -0400

    PasswordResult stores result info and str(x) returns messages like "password: ❌ (#1)"

--- 09-PasswordChecker/my_solution.py
+++ 09-PasswordChecker/my_solution.py
@@ -1,11 +1,25 @@
 class PasswordResult:
 
-    def __init__(self, result: bool, msg=''):
+    def __init__(self, result: bool, *, msg=None, rank=None, password=None):
         self._result = result
-        self._msg = msg
+        self.msg = msg
+        self.rank = rank
+        self._password = password
 
     def __bool__(self) -> bool:
         return self._result
+
+    @property
+    def password(self):
+        return self._password
+
+    def __str__(self):
+        return_str = ""
+        if self._password:
+            return_str = f"{self._password}: "
+        return_str += "✅" if self else "❌"
+        return_str += f" (#{self.rank})" if self.rank else ""
+        return return_str
 
 
 class BadPasswords:
@@ -16,28 +30,26 @@ class BadPasswords:
     def is_ok(self, a_candidate: str) -> bool:
         return a_candidate in self._bad
 
-    def is_ok_and_rank(self, a_candidate: str) -> tuple[bool, str]:
+    def is_ok_and_rank(self, a_candidate: str) -> PasswordResult:
         for i, common_password in enumerate(self._bad, start=1):
             if a_candidate == common_password:
-                return False, str(i)
-        return True, ''
+                return PasswordResult(False,
+                                      password=a_candidate,
+                                      msg="Commonly used password",
+                                      rank=i)
+        return PasswordResult(True, password=a_candidate)
 
 
 def password_check(possible_password: str,
                    list_of_bad: str = "passwords.text") -> PasswordResult:
-    """Return (True, '') if password is okay,
-    otherwise return (False, "reason").
+    """Return PasswordResult"""
 
-    Reasons include:
-        '3' -> 3rd on bad password list
-    """
     try:
         with open(list_of_bad) as file:
             bp = BadPasswords(file.read().splitlines())
     except Exception as _:
         bp = BadPasswords(["password", "passwd", "mike_ulm"])
-    return PasswordResult(*bp.is_ok_and_rank(possible_password))
-
+    return bp.is_ok_and_rank(possible_password)
 
 
 def request_and_check_password():
